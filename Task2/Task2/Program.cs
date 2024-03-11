f2a06d9c999fea3fc315e5e6649ce7c28b23c234
s28706 <s28706@pjwstk.edu.pl>
2024-03-11 16:48:47 +0100

    Modyfikacja 1

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Jesus may anything work in there?!");
+    }
+}
