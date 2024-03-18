547a38279148141fdb50a237cfbb910143138ebc
s28706 <s28706@pjwstk.edu.pl>
2024-03-18 14:40:27 +0100

    example

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static double AvgMethod(int[] array)
+    {
+        int sum = 0;
+        foreach (int val in array)
+        {
+            sum += val;
+        }
+
+        return sum / array.Length;
+    }
+    
+    public static void Main(string[] args)
+    {
+        int[] arr = new[] { 1, 1, 2, 4 };
+        Console.WriteLine(AvgMethod(arr));
+    }
+}
