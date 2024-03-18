abcfed568c9d8557fd10df7d449749dfcf7e77b1
s28706 <s28706@pjwstk.edu.pl>
2024-03-18 15:22:49 +0100

    update main

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -4,16 +4,33 @@
     {
         int[] arr = new[] { 1, 1, 2, 4 };
         Console.WriteLine(AvgMethod(arr));
+        Console.WriteLine(MaxMethod(arr));
     }
 
     public static double AvgMethod(int[] array)
     {
-        int sumSum = 0;
+        int wSumieSumarycznaSumaSumujacaSumeSum = 0;
         foreach(int val in array)
         {
-            sumSum += val;
+            wSumieSumarycznaSumaSumujacaSumeSum += val;
         }
+        // fnaoiooifwaioo
+
+        return wSumieSumarycznaSumaSumujacaSumeSum / array.Length;
+    }
     
-        return sumSum / array.Length;
+    public static double MaxMethod(int[] array)
+    {
+        int max = array[0];
+        
+        foreach(int val in array)
+        {
+            if (max < val)
+            {
+                max = val;
+            }
+        }
+
+        return max;
     }
 }
